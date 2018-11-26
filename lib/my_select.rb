225a7bf96ee389eacfeebc631b2fc353d499fbe7
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-26 21:02:22 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  iterator = 0
+  matching_elements = []
+
+  while iterator < collection.length
+    matching_elements << yield(collection[iterator]) if yield
+    itertor += 1
+  end
+
+
 end
